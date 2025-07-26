d99438bfcc10f540d1d99cfe151a523d9700f9c0
Marti Serra <martiserramolina99@gmail.com>
2025-07-26 11:12:58 +0200

    Removed cursor-pointer

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,7 +3,7 @@ import { tv } from "tailwind-variants";
 import { cn } from "@/utils";
 
 const button = tv({
-  base: "w-full cursor-pointer rounded-xl border px-6 py-2 text-base font-medium text-white transition-opacity hover:opacity-90 focus-visible:ring-2 focus-visible:ring-white focus-visible:outline-none active:opacity-80 disabled:cursor-not-allowed disabled:opacity-50",
+  base: "w-full rounded-xl border px-6 py-2 text-base font-medium text-white transition-opacity hover:opacity-90 focus-visible:ring-2 focus-visible:ring-white focus-visible:outline-none active:opacity-80 disabled:cursor-not-allowed disabled:opacity-50",
   variants: {
     variant: {
       primary: "border-transparent bg-blue-500",
